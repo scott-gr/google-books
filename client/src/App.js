06f353de1e4b2e98e8f1751b7f57e8535a65d09c
Scott Griffin <mscottgriffin@gmail.com>
2020-08-10 15:14:32 -0400

    dir setup

---
+++ client/src/App.js
@@ -0,0 +1,22 @@
+import React, { useEffect } from 'react';
+import axios from 'axios';
+
+function App() {
+  useEffect(() => {
+    axios
+      .get('/api/config')
+      .then((response) => {
+        console.log(response.data);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  }, []);
+  return (
+    <div className="App">
+      <h1> Hello World</h1>
+    </div>
+  );
+}
+
+export default App;
---
+++ server.js
@@ -0,0 +1,30 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const app = express();
+const PORT = process.env.PORT || 3001;
+
+app.use(express.urlencoded({ extended: true}));
+app.use(express.json());
+
+app.get("/api/config", (req, res) => {
+  res.json({
+    success: true,
+  });
+});
+
+mongoose
+  .connect(process.env.MONGODB_URI || "mongodb://localhost/google-books", { 
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then (() => {
+    console.log("Successfully connected to database.");
+  })
+  .catch((err) => {
+    console.log("Unable to connect to database.");
+    console.log(err);
+  });
+
+app.listen(PORT, () => {
+console.log('Express server running on http://localhostL${PORT}');
+});
