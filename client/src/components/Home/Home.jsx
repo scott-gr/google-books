a37ab27d42db0ec6cd2adc50c84296e7a14fa270
Scott Griffin <mscottgriffin@gmail.com>
2020-08-14 04:09:23 -0400

    Homepage component

--- client/src/components/Home/Home.jsx
+++ client/src/components/Home/Home.jsx
@@ -4,7 +4,75 @@ class Home extends Component {
   render() {
     return (
       <div>
-        <h1>This is my home page.</h1>
+        <section class="hero is-primary is-medium">
+
+          <div class="hero-head">
+            <nav class="navbar">
+              <div class="container">
+                <div class="navbar-brand">
+                  <a class="navbar-item">
+                    <img src="https://bulma.io/images/bulma-type-white.png" alt="Logo" />
+                  
+                  <span class="navbar-burger burger" data-target="navbarMenuHeroA">
+                    <span></span>
+                    <span></span>
+                    <span></span>
+                  </span>
+                </a>
+                <div id="navbarMenuHeroA" class="navbar-menu">
+                  <div class="navbar-end">
+                    <a class="navbar-item is-active">
+                      Home
+                    </a>
+                    <a class="navbar-item">
+                      Examples
+                    </a>
+                    <a class="navbar-item">
+                      Documentation
+                    </a>
+                    <span class="navbar-item">
+                      <a class="button is-primary is-inverted">
+                        <span class="icon">
+                          <i class="fab fa-github"></i>
+                        </span>
+                        <span>Download</span>
+                      </a>
+                    </span>
+                  </div>
+                </div>
+              </div>
+            </div>
+            </nav>
+          </div>
+
+
+          <div class="hero-body">
+            <div class="container has-text-centered">
+              <h1 class="title">
+                Title
+              </h1>
+              <h2 class="subtitle">
+                Subtitle
+              </h2>
+            </div>
+          </div>
+
+          <!-- Hero footer: will stick at the bottom -->
+          <div class="hero-foot">
+            <nav class="tabs">
+              <div class="container">
+                <ul>
+                  <li class="is-active"><a>Overview</a></li>
+                  <li><a>Modifiers</a></li>
+                  <li><a>Grid</a></li>
+                  <li><a>Elements</a></li>
+                  <li><a>Components</a></li>
+                  <li><a>Layout</a></li>
+                </ul>
+              </div>
+            </nav>
+          </div>
+        </section>
       </div>
     );
   }
